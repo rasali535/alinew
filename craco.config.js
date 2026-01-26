4efa4e55f4dfdf2113b4df6c055aee0f3e13451b
rasali535 <maplininc@gmail.com>
2026-01-26 21:02:03 +0200

    Revert to Craco structure to match last successful deployment logs. Kept PHP email handler.

---
+++ craco.config.js
@@ -0,0 +1,25 @@
+const CracoAlias = require('craco-alias');
+const path = require('path');
+
+module.exports = {
+    style: {
+        postcss: {
+            plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+            ],
+        },
+    },
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: 'options',
+                baseUrl: './',
+                aliases: {
+                    '@': './src'
+                },
+            },
+        },
+    ],
+};
