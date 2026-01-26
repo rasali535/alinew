b5a8c9cc3a29af4ae0d066f04e8d19d1ba98a708
rasali535 <maplininc@gmail.com>
2026-01-26 17:55:17 +0200

    Fix React hook exhaustive-deps warning in ScrollReveal

--- frontend/src/components/ui/ScrollReveal.jsx
+++ frontend/src/components/ui/ScrollReveal.jsx
@@ -18,13 +18,14 @@ const ScrollReveal = ({ children, className = '', delay = 0 }) => {
             }
         );
 
-        if (ref.current) {
-            observer.observe(ref.current);
+        const currentRef = ref.current;
+        if (currentRef) {
+            observer.observe(currentRef);
         }
 
         return () => {
-            if (ref.current) {
-                observer.unobserve(ref.current);
+            if (currentRef) {
+                observer.unobserve(currentRef);
             }
         };
     }, []);
@@ -33,8 +34,8 @@ const ScrollReveal = ({ children, className = '', delay = 0 }) => {
         <div
             ref={ref}
             className={`transition-all duration-1000 ease-out transform ${isVisible
-                    ? 'opacity-100 translate-y-0'
-                    : 'opacity-0 translate-y-20'
+                ? 'opacity-100 translate-y-0'
+                : 'opacity-0 translate-y-20'
                 } ${className}`}
             style={{ transitionDelay: `${delay}ms` }}
         >
