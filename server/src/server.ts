0c70b1d5ae4079c9ef9cb84df62242e333783ff6
rasali535 <maplininc@gmail.com>
2026-02-17 09:55:12 +0200

    TS Fix: NodeNext resolution and cleanup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import { createApp } from './app.js';
-import { config, validateConfig } from './config/index.js';
+import { config } from './config/index.js';
 import { logger } from './utils/logger.js';
 import { db } from './services/dbService.js';
 import fs from 'fs';
